6269b5e5b8ac1eed443120b7e1cda4761e657be7
sluzhynskyyr <roman.sluzhynskyy@dowjones.com>
2018-12-19 15:34:59 -0500

    add instance option

--- shotty.py
+++ shotty.py
@@ -5,6 +5,7 @@ import botocore
 
 
 session = None
+instance = None
 
 # session = boto3.Session(profile_name="default")
 # ec2 = session.resource('ec2')
@@ -13,20 +14,23 @@ session = None
 @click.option('--profile', default=None, help="Specify AWS profile. OPTIONAL")
 def cli(profile):
     """Manages snapshots"""
-    global session
+    global session, instance
     if profile is None:
         profile = 'default'
     session = boto3.Session(profile_name=profile)
 
 
-def get_instances(project):
+def get_instances(project,instance):
     ec2 = session.resource('ec2')    
     instances = []
     if project:
-    	filters = [{'Name': 'tag:Project', 'Values':[project]}]
-    	instances = ec2.instances.filter(Filters=filters)
+        filters = [{'Name': 'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    if instance:
+        instance = [instance]
+        instances = ec2.instances.filter(InstanceIds=instance)
     else:
-    	instances = ec2.instances.all()
+        instances = ec2.instances.all()
 
     return instances
          
@@ -38,47 +42,52 @@ def has_pending_snapshot(volume):
 
 @cli.group('volumes')
 def volumes():
-	"""Commands for instances"""
+    """Commands for instances"""
 @volumes.command('list')
 @click.option('--project', default=None,
-	help="Only instances for project (tag Project:<name>)")
+    help="Only instances for project (tag Project:<name>)")
+@click.option('--instance', default=None,
+    help="Only for specified instance-id")
 
-def list_volumes(project):
-	"List volumes"
-	instances = get_instances(project)
-	for i in instances:
-		for v in i.volumes.all():
-			print(', '.join((
-				v.id,
+def list_volumes(project, instance):
+    "List volumes"
+
+    instances = get_instances(project, instance)
+    for i in instances:
+        for v in i.volumes.all():
+            print(', '.join((
+                v.id,
                 v.state,
                 str(v.size),
                 str(v.create_time)
-				)))
-	return 
+                               )))
+    return 
 
 
 @cli.group('snapshots')
 def snapshots():
-	"""Commands for snapshots"""
+    """Commands for snapshots"""
 @snapshots.command('list')
 @click.option('--project', default=None,
-	help="Only instances for project (tag Project:<name>)")
+    help="Only instances for project (tag Project:<name>)")
 @click.option('--all', 'list_all', default=False, is_flag=True,
-	help="List all snapshots, not just most recent")
+    help="List all snapshots, not just most recent")
+@click.option('--instance', default=None,
+    help="Only for specified instance-id")
 
-def list_snapshots(project,list_all):
+def list_snapshots(project,list_all,instance):
     "List EC2 snapshots"
-    instances = get_instances(project)
+    instances = get_instances(project,instance)
     for i in instances:
         for v in i.volumes.all():
             for s in v.snapshots.all():
                 print(', '.join((
-                	s.id,
-                	s.volume_id,
-                	s.progress,
-                	s.state,
-                	s.start_time.strftime("%c")
-                	)))
+                    s.id,
+                    s.volume_id,
+                    s.progress,
+                    s.state,
+                    s.start_time.strftime("%c")
+                    )))
 
                 if s.state == 'completed' and not list_all: break
 
@@ -86,66 +95,68 @@ def list_snapshots(project,list_all):
 
 @cli.group('instances')
 def instances():
-	"""Commands for instances"""
+    """Commands for instances"""
 
 @instances.command('list')
 @click.option('--project', default=None,
-	help="Only instances for project (tag Project:<name>)")
+    help="Only instances for project (tag Project:<name>)")
 
 def list_instances(project):
     "List EC2 instances"
-    instances = get_instances(project)
+    instances = get_instances(project,None)
 
     for i in instances:
-    	tags = { t['Key']: t['Value'] for t in i.tags or [] }
-    	print(', '.join((
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
+        print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
             i.public_dns_name,
             tags.get('Project', '<no project>')
-	    	)))
+        )))
     return
 
 
 @instances.command('stop')
 @click.option('--project', default=None,
-	help="Only instances for project (tag Project:<name>)")
-
+    help="Only instances for project (tag Project:<name>)")
 @click.option('--force', 'force', default=False, is_flag=True,
     help="Force reboot")
+@click.option('--instance', default=None,
+    help="Only for specified instance-id")
 
-def stop_instances(project,force):
+def stop_instances(project,force,instance):
     "Stop EC2 instances"
-    instances = get_instances(project)
+    instances = get_instances(project,instance)
 
-    if not force and not project:
-        print("ERR: You need to specify --force or --project option in order to reboot instances")
+    if not force and not project and not instance:
+        print("ERR: You need to specify --force or --project or --instance option in order to reboot instances")
         exit(2)
 
     for i in instances:
-    	print("Stopping {0}...".format(i.id))
-    	try:
-    		i.stop()
-    	except botocore.exceptions.ClientError as e:
-    		print ("Could not stop {0} ".format(i.id) + str(e))
-    		continue
+        print("Stopping {0}...".format(i.id))
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print ("Could not stop {0} ".format(i.id) + str(e))
+            continue
     return
 
 
 @instances.command('reboot')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>)")
-
 @click.option('--force', 'force', default=False, is_flag=True,
     help="Force reboot")
+@click.option('--instance', default=None,
+    help="Only for specified instance-id")
 
-def reboot_instances(project,force):
+def reboot_instances(project,force,instance):
     "Reboot EC2 instances"
-    instances = get_instances(project)
-    if not force and not project:
-        print("ERR: You need ot specify --force or --project option in order to reboot instances")
+    instances = get_instances(project,instance)
+    if not force and not project and not instance:
+        print("ERR: You need ot specify --force or --project or --instance option in order to reboot instances")
         exit(2)
 
     for i in instances:
@@ -162,22 +173,24 @@ def reboot_instances(project,force):
 	help="Only instances for project (tag Project:<name>)")
 @click.option('--force', 'force', default=False, is_flag=True,
     help="Force reboot")
+@click.option('--instance', default=None,
+    help="Only for specified instance-id")
 
-def start_instances(project,force):
+def start_instances(project,force,instance):
     "Start EC2 instances"
-    instances = get_instances(project)
+    instances = get_instances(project,instance)
 
-    if not force and not project:
-        print("ERR: You need ot specify --force or --project option in order to start instances")
+    if not force and not project and not instance:
+        print("ERR: You need ot specify --force or --project or --instance option in order to start instances")
         exit(2)
 
     for i in instances:
-    	print("Starting {0}...".format(i.id))
-    	try:
-    		i.start()
-    	except botocore.exceptions.ClientError as e:
-    	    print ("Could not start {0} ".format(i.id) + str(e))
-    	    continue
+        print("Starting {0}...".format(i.id))
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print ("Could not start {0} ".format(i.id) + str(e))
+        continue
 
     return
 
@@ -186,14 +199,16 @@ def start_instances(project,force):
     help="Only instances for project (tag Project:<name>)")
 @click.option('--force', 'force', default=False, is_flag=True,
     help="Force reboot")
+@click.option('--instance', default=None,
+    help="Only for specified instance-id")
 
 
-def create_snapshot(project, force):
+def create_snapshot(project, force, instance):
     "Create snapshots"
-    instances = get_instances(project)
+    instances = get_instances(project,instance)
 
-    if not force and not project:
-        print("ERR: You need ot specify --force or --project option in order to start instances")
+    if not force and not project and not instance:
+        print("ERR: You need ot specify --force or --project or --instance option in order to start instances")
         exit(2)
 
     for i in instances:
